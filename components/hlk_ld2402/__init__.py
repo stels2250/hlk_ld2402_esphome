ad001865f8270adfbe42ff0b7e110739a0552605
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-03-28 19:51:18 +0800

    fix

--- components/hlk_ld2402/__init__.py
+++ components/hlk_ld2402/__init__.py
@@ -1,62 +1,62 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, binary_sensor, sensor
+from esphome.components import uart, sensor, binary_sensor
 from esphome.const import CONF_ID
 
-# Required ESPHome declarations
-CODEOWNERS = ["@mouldybread"]
-DEPENDENCIES = ["uart"]
-AUTO_LOAD = ["binary_sensor", "sensor"]
+DEPENDENCIES = ['uart']
+AUTO_LOAD = ['binary_sensor', 'sensor']
+CODEOWNERS = ['@mouldybread']
 
-# Configuration variables
-CONF_HLK_LD2402 = "hlk_ld2402"
-CONF_PRESENCE = "presence"
-CONF_DISTANCE = "distance"
-CONF_MOVEMENT = "movement"
-CONF_MICROMOVEMENT = "micromovement"
-CONF_MAX_DISTANCE = "max_distance"
-CONF_DISAPPEAR_DELAY = "disappear_delay"
+hlk_ld2402_ns = cg.esphome_ns.namespace('hlk_ld2402')
+HLKLD2402 = hlk_ld2402_ns.class_('HLKLD2402', cg.Component, uart.UARTDevice)
 
-# Namespace setup
-hlk_ld2402_ns = cg.esphome_ns.namespace("hlk_ld2402")
-HLKLD2402Component = hlk_ld2402_ns.class_(
-    "HLKLD2402Component", cg.Component, uart.UARTDevice
-)
+# Configuration for the main component
+CONF_HLK_LD2402 = 'hlk_ld2402'
+CONF_MAX_DISTANCE = 'max_distance'
+CONF_DISAPPEAR_DELAY = 'disappear_delay'
 
-# Sensor schemas
-PRESENCE_SCHEMA = binary_sensor.binary_sensor_schema(device_class="presence")
-MOVEMENT_SCHEMA = binary_sensor.binary_sensor_schema()
-MICROMOVEMENT_SCHEMA = binary_sensor.binary_sensor_schema()
-DISTANCE_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement="m",
-    accuracy_decimals=2,
-)
-
-# Main component schema
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(HLKLD2402Component),
+HLK_LD2402_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(HLKLD2402),
     cv.Optional(CONF_MAX_DISTANCE, default=8.5): cv.float_range(min=0.7, max=10.0),
     cv.Optional(CONF_DISAPPEAR_DELAY, default=30): cv.uint16_t,
-    cv.Optional(CONF_PRESENCE): PRESENCE_SCHEMA,
-    cv.Optional(CONF_DISTANCE): DISTANCE_SCHEMA,
-    cv.Optional(CONF_MOVEMENT): MOVEMENT_SCHEMA,
-    cv.Optional(CONF_MICROMOVEMENT): MICROMOVEMENT_SCHEMA,
 }).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
+# Configuration for sensors
+CONF_DISTANCE = 'distance'
+CONF_PRESENCE = 'presence'
+CONF_MOVEMENT = 'movement'
+CONF_MICROMOVEMENT = 'micromovement'
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.Optional(CONF_HLK_LD2402): HLK_LD2402_SCHEMA,
+    cv.Optional(CONF_DISTANCE): sensor.sensor_schema(
+        unit_of_measurement="m",
+        accuracy_decimals=2,
+    ),
+    cv.Optional(CONF_PRESENCE): binary_sensor.binary_sensor_schema(
+        device_class="presence"
+    ),
+    cv.Optional(CONF_MOVEMENT): binary_sensor.binary_sensor_schema(),
+    cv.Optional(CONF_MICROMOVEMENT): binary_sensor.binary_sensor_schema(),
+})
+
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
+    # Main component
+    if CONF_HLK_LD2402 in config:
+        var = cg.new_Pvariable(config[CONF_HLK_LD2402][CONF_ID])
+        await cg.register_component(var, config[CONF_HLK_LD2402])
+        await uart.register_uart_device(var, config[CONF_HLK_LD2402])
+        
+        cg.add(var.set_max_distance(config[CONF_HLK_LD2402][CONF_MAX_DISTANCE]))
+        cg.add(var.set_disappear_delay(config[CONF_HLK_LD2402][CONF_DISAPPEAR_DELAY]))
     
-    cg.add(var.set_max_distance(config[CONF_MAX_DISTANCE]))
-    cg.add(var.set_disappear_delay(config[CONF_DISAPPEAR_DELAY]))
-    
-    if CONF_PRESENCE in config:
-        sens = await binary_sensor.new_binary_sensor(config[CONF_PRESENCE])
-        cg.add(var.set_presence_sensor(sens))
+    # Sensors
     if CONF_DISTANCE in config:
         sens = await sensor.new_sensor(config[CONF_DISTANCE])
         cg.add(var.set_distance_sensor(sens))
+    if CONF_PRESENCE in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_PRESENCE])
+        cg.add(var.set_presence_sensor(sens))
     if CONF_MOVEMENT in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_MOVEMENT])
         cg.add(var.set_movement_sensor(sens))
