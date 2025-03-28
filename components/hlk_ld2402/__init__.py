90104dbd99557a263fd20ded4fc2f6643a8bca1a
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-03-28 23:12:16 +0800

    Update __init__.py

--- components/hlk_ld2402/__init__.py
+++ components/hlk_ld2402/__init__.py
@@ -23,25 +23,20 @@ CONF_PRESENCE = "presence"
 CONF_MAX_DISTANCE = "max_distance"
 CONF_TIMEOUT = "timeout"
 
-HLKLD2402_SENSOR_SCHEMA = cv.Schema(
-    {
-        cv.Optional(CONF_DISTANCE): sensor.sensor_schema(
-            device_class=DEVICE_CLASS_DISTANCE,
-            state_class=STATE_CLASS_MEASUREMENT,
-            unit_of_measurement=UNIT_CENTIMETER,
-            accuracy_decimals=0,
-        ),
-        cv.Optional(CONF_PRESENCE): binary_sensor.binary_sensor_schema(
-            device_class=DEVICE_CLASS_PRESENCE,
-        ),
-        cv.Optional(CONF_MAX_DISTANCE, default=10.0): cv.float_range(min=0.7, max=10.0),
-        cv.Optional(CONF_TIMEOUT, default=5): cv.int_range(min=0, max=65535),
-    }
-)
-
-CONFIG_SCHEMA = cv.All(
-    HLKLD2402_SENSOR_SCHEMA.extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
-)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(HLKLD2402Component),
+    cv.Optional(CONF_DISTANCE): sensor.sensor_schema(
+        device_class=DEVICE_CLASS_DISTANCE,
+        state_class=STATE_CLASS_MEASUREMENT,
+        unit_of_measurement=UNIT_CENTIMETER,
+        accuracy_decimals=0,
+    ),
+    cv.Optional(CONF_PRESENCE): binary_sensor.binary_sensor_schema(
+        device_class=DEVICE_CLASS_PRESENCE,
+    ),
+    cv.Optional(CONF_MAX_DISTANCE, default=10.0): cv.float_range(min=0.7, max=10.0),
+    cv.Optional(CONF_TIMEOUT, default=5): cv.int_range(min=0, max=65535),
+}).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
