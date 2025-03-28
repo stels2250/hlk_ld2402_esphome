6e114f49d13f1457606862e46c2fb6703ee6c36a
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-03-28 20:20:12 +0800

    fix

--- components/binary_sensor/hlk_ld2402.py
+++ components/binary_sensor/hlk_ld2402.py
@@ -1,12 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID, CONF_TYPE, CONF_DEVICE_CLASS
+from esphome.const import CONF_ID, CONF_HLK_LD2402_ID, CONF_TYPE
 
 from .. import hlk_ld2402_ns, HLKLD2402Component
 
 DEPENDENCIES = ["hlk_ld2402"]
-CONF_HLK_LD2402 = "hlk_ld2402_id"
+AUTO_LOAD = ["hlk_ld2402"]
 
 CONF_PRESENCE = "presence"
 CONF_MOVEMENT = "movement"
@@ -16,27 +16,28 @@ HLKLD2402BinarySensor = hlk_ld2402_ns.class_(
     "HLKLD2402BinarySensor", binary_sensor.BinarySensor, cg.Component
 )
 
-def validate_config(value):
+def validate_binary_sensor(value):
     value = cv.Schema({
         cv.GenerateID(): cv.declare_id(HLKLD2402BinarySensor),
-        cv.Required(CONF_HLK_LD2402): cv.use_id(HLKLD2402Component),
+        cv.GenerateID(CONF_HLK_LD2402_ID): cv.use_id(HLKLD2402Component),
         cv.Required(CONF_TYPE): cv.one_of(
             CONF_PRESENCE, CONF_MOVEMENT, CONF_MICROMOVEMENT, lower=True
         ),
-        cv.Optional(CONF_DEVICE_CLASS): cv.string,
     })(value)
     
-    if value[CONF_TYPE] == CONF_PRESENCE and CONF_DEVICE_CLASS not in value:
-        value[CONF_DEVICE_CLASS] = "presence"
+    if value[CONF_TYPE] == CONF_PRESENCE:
+        value = binary_sensor.binary_sensor_schema(
+            device_class="presence"
+        )(value)
     return value
 
-CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend(validate_config)
+CONFIG_SCHEMA = validate_binary_sensor
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await binary_sensor.register_binary_sensor(var, config)
     
-    parent = await cg.get_variable(config[CONF_HLK_LD2402])
+    parent = await cg.get_variable(config[CONF_HLK_LD2402_ID])
     cg.add(var.set_parent(parent))
     cg.add(var.set_type(config[CONF_TYPE]))
--- components/sensor/hlk_ld2402.py
+++ components/sensor/hlk_ld2402.py
@@ -1,26 +1,31 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_HLK_LD2402_ID
 
 from .. import hlk_ld2402_ns, HLKLD2402Component
 
 DEPENDENCIES = ["hlk_ld2402"]
-CONF_HLK_LD2402 = "hlk_ld2402_id"
+AUTO_LOAD = ["hlk_ld2402"]
+
+CONF_DISTANCE = "distance"
 
 HLKLD2402Sensor = hlk_ld2402_ns.class_(
     "HLKLD2402Sensor", sensor.Sensor, cg.Component
 )
 
-CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(HLKLD2402Sensor),
-    cv.GenerateID(CONF_HLK_LD2402): cv.use_id(HLKLD2402Component),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = sensor.sensor_schema(
+    HLKLD2402Sensor,
+    unit_of_measurement="m",
+    accuracy_decimals=2,
+).extend({
+    cv.GenerateID(CONF_HLK_LD2402_ID): cv.use_id(HLKLD2402Component),
+})
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
     
-    parent = await cg.get_variable(config[CONF_HLK_LD2402])
+    parent = await cg.get_variable(config[CONF_HLK_LD2402_ID])
     cg.add(var.set_parent(parent))
