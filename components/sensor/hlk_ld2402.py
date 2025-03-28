2fb5ba4ac9354ce8c3ac7cfa29194f1474384ca9
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-03-28 20:23:09 +0800

    fix

--- components/sensor/hlk_ld2402.py
+++ components/sensor/hlk_ld2402.py
@@ -1,26 +1,24 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import pins
 from esphome.components import sensor
-from esphome.const import CONF_ID, CONF_HLK_LD2402_ID
+from esphome.const import CONF_ID
 
 from .. import hlk_ld2402_ns, HLKLD2402Component
 
-DEPENDENCIES = ["hlk_ld2402"]
-AUTO_LOAD = ["hlk_ld2402"]
+DEPENDENCIES = ['hlk_ld2402']
+AUTO_LOAD = ['hlk_ld2402']
 
-CONF_DISTANCE = "distance"
+CONF_HLK_LD2402_ID = "hlk_ld2402_id"
 
 HLKLD2402Sensor = hlk_ld2402_ns.class_(
     "HLKLD2402Sensor", sensor.Sensor, cg.Component
 )
 
-CONFIG_SCHEMA = sensor.sensor_schema(
-    HLKLD2402Sensor,
-    unit_of_measurement="m",
-    accuracy_decimals=2,
-).extend({
-    cv.GenerateID(CONF_HLK_LD2402_ID): cv.use_id(HLKLD2402Component),
-})
+CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(HLKLD2402Sensor),
+    cv.Required(CONF_HLK_LD2402_ID): cv.use_id(HLKLD2402Component),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
