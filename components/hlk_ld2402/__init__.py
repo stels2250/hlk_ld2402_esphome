c809a9e0d9703131df7bf387bf7fb4d8b8618aa6
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-03-29 16:09:52 +0800

    firmware sensor

--- components/hlk_ld2402/__init__.py
+++ components/hlk_ld2402/__init__.py
@@ -1,15 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart
-from esphome.const import CONF_ID, CONF_TIMEOUT
+from esphome.components import uart, text_sensor
+from esphome.const import CONF_ID, CONF_TIMEOUT, ENTITY_CATEGORY_DIAGNOSTIC
 
 DEPENDENCIES = ["uart"]
-# Add text_sensor to AUTO_LOAD
-AUTO_LOAD = ["sensor", "binary_sensor", "text_sensor"]
+AUTO_LOAD = ["sensor", "binary_sensor"]  # Remove text_sensor from AUTO_LOAD
 
 # Define our own constants
 CONF_MAX_DISTANCE = "max_distance"
 CONF_HLK_LD2402_ID = "hlk_ld2402_id"
+CONF_FIRMWARE_VERSION = "firmware_version_sensor"  # New config option
 
 hlk_ld2402_ns = cg.esphome_ns.namespace("hlk_ld2402")
 HLKLD2402Component = hlk_ld2402_ns.class_(
@@ -21,6 +21,13 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(HLKLD2402Component),
     cv.Optional(CONF_MAX_DISTANCE, default=5.0): cv.float_range(min=0.7, max=10.0),
     cv.Optional(CONF_TIMEOUT, default=5): cv.int_range(min=0, max=65535),
+    cv.Optional(CONF_FIRMWARE_VERSION): cv.maybe_simple_value(
+        text_sensor.TEXT_SENSOR_SCHEMA.extend({
+            cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+            cv.Optional("entity_category", default=ENTITY_CATEGORY_DIAGNOSTIC): cv.string,
+        }),
+        key=CONF_ID,
+    ),
 }).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
@@ -32,3 +39,9 @@ async def to_code(config):
         cg.add(var.set_max_distance(config[CONF_MAX_DISTANCE]))
     if CONF_TIMEOUT in config:
         cg.add(var.set_timeout(config[CONF_TIMEOUT]))
+    
+    # Add firmware version text sensor if configured
+    if CONF_FIRMWARE_VERSION in config:
+        fw_config = config[CONF_FIRMWARE_VERSION]
+        fw_sensor = await text_sensor.new_text_sensor(fw_config)
+        cg.add(var.set_firmware_version_text_sensor(fw_sensor))
