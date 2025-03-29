d550c9e6f8b894482c8f0fbf349312816f54fd87
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-03-29 20:18:00 +0800

    Update sensor.py

--- components/hlk_ld2402/sensor.py
+++ components/hlk_ld2402/sensor.py
@@ -15,42 +15,23 @@ from . import HLKLD2402Component, CONF_HLK_LD2402_ID
 CONF_THROTTLE = "throttle"
 CONF_CALIBRATION_PROGRESS = "calibration_progress"
 
-# Create standard distance sensor schema
-DISTANCE_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement=UNIT_CENTIMETER,
-    accuracy_decimals=1,
-    device_class=DEVICE_CLASS_DISTANCE,
-    state_class=STATE_CLASS_MEASUREMENT,
-).extend({
+# Define a single schema with optional fields for both sensor types
+CONFIG_SCHEMA = sensor.sensor_schema().extend({
     cv.GenerateID(): cv.declare_id(sensor.Sensor),
     cv.Required(CONF_HLK_LD2402_ID): cv.use_id(HLKLD2402Component),
     cv.Optional(CONF_THROTTLE): cv.positive_time_period_milliseconds,
-})
-
-# Create calibration progress sensor schema
-CALIBRATION_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement=UNIT_PERCENT,
-    accuracy_decimals=0,
-    state_class=STATE_CLASS_DIAGNOSTIC,
-    icon="mdi:progress-wrench"
-).extend({
-    cv.GenerateID(): cv.declare_id(sensor.Sensor),
-    cv.Required(CONF_HLK_LD2402_ID): cv.use_id(HLKLD2402Component),
-    cv.Optional(CONF_CALIBRATION_PROGRESS, default=True): cv.boolean,
-})
-
-CONFIG_SCHEMA = cv.typed_schema({
-    CONF_CALIBRATION_PROGRESS: CALIBRATION_SCHEMA,
-    cv.CONF_EMPTY: DISTANCE_SCHEMA,  # Default to distance sensor
+    cv.Optional(CONF_CALIBRATION_PROGRESS, default=False): cv.boolean,
 })
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_HLK_LD2402_ID])
     var = await sensor.new_sensor(config)
     
-    if CONF_CALIBRATION_PROGRESS in config:
+    if config.get(CONF_CALIBRATION_PROGRESS):
+        # This is a calibration progress sensor
         cg.add(parent.set_calibration_progress_sensor(var))
-    else:  # Regular distance sensor
+    else:
+        # This is a regular distance sensor
         cg.add(parent.set_distance_sensor(var))
         if CONF_THROTTLE in config:
             cg.add(parent.set_distance_throttle(config[CONF_THROTTLE]))
