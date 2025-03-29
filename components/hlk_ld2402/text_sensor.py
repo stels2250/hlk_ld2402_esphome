1b62c29f6ddf45d6d2914da07135a139316a95a0
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-03-29 16:51:49 +0800

    fix

---
+++ components/hlk_ld2402/text_sensor.py
@@ -0,0 +1,21 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import text_sensor
+from esphome.const import (
+    CONF_ID,
+    ENTITY_CATEGORY_DIAGNOSTIC,
+)
+
+from . import HLKLD2402Component, CONF_HLK_LD2402_ID
+
+CONFIG_SCHEMA = text_sensor.text_sensor_schema(
+    entity_category=ENTITY_CATEGORY_DIAGNOSTIC
+).extend({
+    cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+    cv.Required(CONF_HLK_LD2402_ID): cv.use_id(HLKLD2402Component),
+})
+
+async def to_code(config):
+    parent = await cg.get_variable(config[CONF_HLK_LD2402_ID])
+    var = await text_sensor.new_text_sensor(config)
+    cg.add(parent.set_firmware_version_text_sensor(var))
